b31b9626e2c23eeaf1609347b1a059cb3428a014
Jonathan <jonsandersss@gmail.com>
2020-05-15 11:19:23 -0400

    redirect from quiz if no questions yet

--- src/scenes/Quiz.js
+++ src/scenes/Quiz.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useContext } from "react";
+import { store } from "../store";
+import { Button } from "semantic-ui-react";
+import { Redirect } from "react-router-dom";
 
 const Quiz = () => {
-  return <div>Quiz</div>;
+  const { state } = useContext(store);
+  if (state.questions.length === 0) {
+    return <Redirect to="/home" />;
+  }
+
+  return (
+    <div>
+      <Button.Group>
+        <Button>False</Button>
+        <Button.Or />
+        <Button positive>True</Button>
+      </Button.Group>
+    </div>
+  );
 };
 
 export default Quiz;
